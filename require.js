2d4fa31e11b3019cd9139cd0d4361a4cfc1ec959
Bastian Müller <bastian@turbolent.com>
2011-07-21 12:03:22 +0200

    require: load every script in own iframe (new context)

--- require.js
+++ require.js
@@ -2,7 +2,7 @@
 // define('module-name', [dependeny, ...], function (exports) { ... })
 // require(['module-name', ...], function (exports) { ... })
 
-var require, define;
+var require, define, contexts = {};
 
 (function () {
     var modules = {};
@@ -15,11 +15,20 @@ var require, define;
 	function load (name, callback) {
 		if (!callbacks[name]) {
 			callbacks[name] = [callback];
+			var url = root + name + '.js';
 
-			console.log("load", name, +new Date());
-			var script = document.createElement('script');
-			script.setAttribute('src', root + name + '.js');
-			head.appendChild(script);
+			var timestamp = (new Date()).getTime();
+			url += '?time=' + timestamp;
+
+			var iframe = document.createElement('iframe');
+			iframe.style.display = 'none';
+			document.body.appendChild(iframe);
+			iframe.contentWindow.eval("(function () {"
+									  + "var script = document.createElement('script');"
+									  + "script.setAttribute('src', '" + url + "');"
+									  + "document.head.appendChild(script);"
+									  + "})()");
+			contexts[name] = iframe.contentWindow;
 		} else
 			callbacks[name].push(callback);
 	};
@@ -45,10 +54,23 @@ var require, define;
 		})();
 	};
 
+	function inject (module, dependencies) {
+		for (var i = 0, n = dependencies.length; i < n; i++) {
+			var dependency = dependencies[i];
+			var importedModule = modules[dependency];
+			for (var property in importedModule) {
+				if (importedModule.hasOwnProperty(property)) {
+					module[property] = importedModule[property];
+				}
+			}
+		}
+	}
+
 	define = function (name, dependencies, factory) {
 		(function check () {
 			if (ensureLoaded(dependencies, check)) {
 				var module = {};
+				inject(contexts[name], dependencies);
 				factory(module);
 				modules[name] = module;
 				callbacks[name].forEach(function (callback) {
